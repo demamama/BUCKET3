0b1ca8b7c8db8d753e1d0d1582b27307132cf7a1
demamama <82538255+demamama@users.noreply.github.com>
2021-04-22 19:57:39 +0300

    Add files via upload

---
+++ Bucket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.TryGetComponent(out Rubbish rubbish))
+        {
+            Destroy(rubbish.gameObject);
+        }
+    }
+}
